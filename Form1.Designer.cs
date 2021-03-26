977ac835afee78782140d27df20864756f6bf15f
varsha verma <varsha.verma85@gmail.com>
2021-03-26 18:03:55 +0100

    initial testepool

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace testCasePool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("root");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "";
+            treeNode1.Text = "root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(201, 452);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mouseDouble_click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(207, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Node";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(207, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Remove node";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(207, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Del Row";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "id";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "name";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(307, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 374);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Text = "dataGridView1";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(207, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "DB Connection";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(357, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(502, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "new node";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 451);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace testCasePool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataBaseConnection();
+        }
+        
+
+        private void DataBaseConnection()
+        {
+
+            string fullPath = @"URI=file:C:\work\csharpproject\testCasePool\headsets.db";
+            SQLiteConnection conread = new SQLiteConnection(fullPath);
+            conread.Open();
+
+            string selectSQL = "SELECT * FROM test";
+            SQLiteCommand selectCommand = new SQLiteCommand(selectSQL, conread);
+            SQLiteDataReader dataReader = selectCommand.ExecuteReader();
+            
+            while (dataReader.Read())
+            {
+               
+               var Id = Convert.ToInt32(dataReader["test_id"]);
+               var title = dataReader["title"].ToString();
+               //var price = Convert.ToInt32(dataReader["price"]);
+
+                var n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = Id;
+                dataGridView1.Rows[n].Cells[1].Value = title;
+                //dataGridView1.Rows[n].Cells[2].Value = price;
+
+
+            }
+
+            conread.Close();
+
+        }    
+        private void button1_Click(object sender, EventArgs e)
+        {
+             TreeNode t1 = treeView1.Nodes.Add("ON_oFF");
+             TreeNode t2 = treeView1.Nodes.Add("USB");
+             TreeNode t3 = treeView1.Nodes.Add("play_pause");
+             t1.Nodes.Add("ON_testCase_1");
+             t1.Nodes.Add("ON_testcase_2");
+             t1.Nodes.Add("ON_testCase_3");
+             t1.Nodes.Add("OFF_testCase_4");
+             t1.Nodes.Add("OFF_testcase_1");
+             t1.Nodes.Add("OFF_testCase_2");
+             t2.Nodes.Add("USB_testCase_1");
+             t2.Nodes.Add("USB_testcase_2");
+             t2.Nodes.Add("USB_testCase_3");
+             t2.Nodes.Add("USB_testCase_4");
+
+
+             t3.Nodes.Add("Pause_testCase_1");
+             t3.Nodes.Add("Pause_testCase_2");
+             t3.Nodes.Add("Pause_testCase_3");
+             t3.Nodes.Add("Play_testCase_1");
+             t3.Nodes.Add("Play_testCase_2");
+             t3.Nodes.Add("Play_testCase_3"); 
+
+            
+
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //treeView1.SelectedNode.Remove();
+            RemoveCheckedNodes(treeView1.Nodes);
+        }
+
+        List<TreeNode> checkedNodes = new List<TreeNode>();
+
+        void RemoveCheckedNodes(TreeNodeCollection nodes)
+        {
+            foreach(TreeNode node in nodes)
+            {
+                if (node.Checked)
+                {
+                    checkedNodes.Add(node);
+
+                }
+                else
+                {
+                    RemoveCheckedNodes(node.Nodes);
+                }
+            }
+            foreach (TreeNode checkedNode in checkedNodes)
+            {
+                nodes.Remove(checkedNode);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                if (!row.IsNewRow) dataGridView1.Rows.Remove(row);
+
+
+            }
+                    
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode(textBox1.Text);
+            try
+            {
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+            catch (Exception ex)
+            {
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+
+        }
+
+        private void mouseDouble_click(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                string text = treeView1.SelectedNode.Text;
+                //MessageBox.Show(text);
+                string fullPath = @"URI=file:C:\work\csharpproject\testCasePool\headsets.db";
+                SQLiteConnection conread = new SQLiteConnection(fullPath);
+                conread.Open();
+
+                string selectSQL = string.Format("SELECT * FROM test WHERE label1='{0}'", text.ToLower());
+
+                SQLiteCommand selectCommand = new SQLiteCommand(selectSQL, conread);
+                SQLiteDataReader dataReader = selectCommand.ExecuteReader();
+                while (dataReader.Read())
+                {
+
+                    var Id = Convert.ToInt32(dataReader["test_id"]);
+                    var title = dataReader["title"].ToString();
+                    //var price = Convert.ToInt32(dataReader["price"]);
+                    dataGridView1.Rows.Clear();
+                    var n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[0].Value = Id;
+                    dataGridView1.Rows[n].Cells[1].Value = title;
+                    //dataGridView1.Rows[n].Cells[2].Value = price;
+
+
+                }
+            }
+
+            catch(Exception ex)
+
+            {
+                MessageBox.Show("exeception happened");
+            }
+        }
+    }
+}
